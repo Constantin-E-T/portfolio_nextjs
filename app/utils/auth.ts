ff3f753971a5aced6150500432ad09110f03c0c6
Constantin ET <57395630+Constantin-E-T@users.noreply.github.com>
2025-02-01 19:12:18 +0000

    auth test prod

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,13 +1,32 @@
-// app/utils/auth.ts
-
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./db"
 
- 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter( prisma ),
-  providers: [ GitHub, Google ],
-})  
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    })
+  ],
+  trustHost: true,
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true,
+        domain: '.serverplus.org' // Adjust this to match your domain
+      }
+    }
+  }
+})
