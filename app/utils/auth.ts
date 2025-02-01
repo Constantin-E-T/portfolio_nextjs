346866122b1a66c5a32981be68e615f536c7cbb2
Constantin ET <57395630+Constantin-E-T@users.noreply.github.com>
2025-02-01 19:24:14 +0000

    auth test prod

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -4,12 +4,22 @@ import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./db"
 
+// Parse the NEXTAUTH_URLS from environment variable
+const allowedUrls = process.env.NEXTAUTH_URLS ? 
+  JSON.parse(process.env.NEXTAUTH_URLS) : 
+  [];
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID!,
       clientSecret: process.env.AUTH_GITHUB_SECRET!,
+      authorization: {
+        params: {
+          redirect_uri: process.env.NEXTAUTH_URL + "/api/auth/callback/github"
+        }
+      }
     }),
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
@@ -17,16 +27,19 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     })
   ],
   trustHost: true,
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: true,
-        domain: '.serverplus.org' // Adjust this to match your domain
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      const allowedDomains = [process.env.NEXTAUTH_URL, ...allowedUrls];
+      
+      if (url.startsWith('/')) {
+        return `${baseUrl}${url}`;
+      } 
+      
+      if (allowedDomains.some(domain => url.startsWith(domain))) {
+        return url;
       }
+      
+      return baseUrl;
     }
   }
 })
