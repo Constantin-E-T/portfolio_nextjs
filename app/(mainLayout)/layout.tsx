7b0130ccffdcc41ca6ba0b93ba679f1652ce811d
Constantin ET <57395630+Constantin-E-T@users.noreply.github.com>
2025-02-01 02:18:04 +0000

    auth test prod

--- app/(mainLayout)/layout.tsx
+++ app/(mainLayout)/layout.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode } from 'react';
 import { Navbar } from '@/app/components/layout/Navbar';
-import { Footer } from '@/app/components/layout/Footer';  // Update this path
+import { Footer } from '@/components/layout/Footer';  
 
 interface MainLayoutProps {
     children: ReactNode;
